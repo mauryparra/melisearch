c13092d7c4e5a2245a5d41430481263e84d56313
Mauricio Parra Casado <mauryparra@gmail.com>
2019-05-23 17:38:07 -0300

    El response tiene 50% probabilidad de responder con error

--- src/melisearch/controllers/ping/ping_controller.go
+++ src/melisearch/controllers/ping/ping_controller.go
@@ -1,9 +1,22 @@
 package ping
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"math/rand"
+
+	"github.com/gin-gonic/gin"
+)
 
 // Ping answers with string "pong"
 func Ping(context *gin.Context) {
 
-	context.String(200, "pong")
+	opcion := rand.Intn(2)
+
+	switch opcion {
+	case 0:
+		context.AbortWithError(500, errors.New("Error en api final"))
+	case 1:
+		context.String(200, "Pong API")
+	}
+
 }
